d90051e384f8dd98e822e73b7119abd7a7c74be2
Christoph Tillmann <christoph.tillmann@basf.com>
2021-05-17 14:07:07 +0200

    Added TranslateModule to components import array

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { AppModule } from '../app.module';
+
+export function createHomeTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    TranslateModule.forChild(
+      {
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (createHomeTranslateLoader),
+          deps: [HttpClient]
+        },        
+      }
+    )
   ],
   exports: [
     HomeComponent
